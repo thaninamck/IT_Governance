9842c4a70a6678521506037ceca6fef4b479cea4
Thanina MECHERAK <thanina.mecherak@mazars.dz>
2025-01-21 09:35:52 +0100

    folders

---
+++ frontend/src/components/PopUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PopUp = () => {
+  return (
+    <div className='bg-red-500 w-[779px] h-[407px] justify-center align-middle'>
+      hola everyone
+    </div>
+  );
+};
+
+export default PopUp;
