d0a9797e338780520f8b3139718030e54935ae24
Thanina MECHERAK <thanina.mecherak@mazars.dz>
2025-01-21 10:27:59 +0100

    colors settings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import PopUp from './components/PopUp';
 import './App.css'
-
+import './index.css'
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-       <h1 >
-      Hello world!
-    </h1>
+      <PopUp >
+     
+      </PopUp>
+      
     </>
+
   )
 }
 
