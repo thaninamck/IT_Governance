4509ac8015fc9d1f417ffdc0eb914dd91c1590b1
Manel MOHAND OUALI <manel.mohandouali@mazars.dz>
2025-01-22 09:29:03 +0100

    create Icons file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import PopUp from './components/PopUp';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import './index.css'
+import SideBar from './components/sideBar/SideBar';
+import Header from './components/Header/Header';
+import HeaderBis from './components/Header/HeaderBis';
 function App() {
   
 
@@ -11,6 +14,10 @@ function App() {
        <div className="App">
         <Routes>
           <Route path='/popup' element={<PopUp/>}/>
+          <Route path='/sideBar' element={<SideBar userRole="admin"/>}/>
+          <Route path='/header' element={<Header/>}/>
+          <Route path='/headerbis' element={<HeaderBis/>}/>
+          
           
         </Routes>
         
---
+++ frontend/src/assets/Icons.js
@@ -0,0 +1,36 @@
+// icons de sideBar
+import PersonOutlineRoundedIcon from '@mui/icons-material/PersonOutlineRounded';
+import FeedOutlinedIcon from '@mui/icons-material/FeedOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import GradingRoundedIcon from '@mui/icons-material/GradingRounded';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded';
+import PeopleAltRoundedIcon from '@mui/icons-material/PeopleAltRounded';
+import CollectionsBookmarkRoundedIcon from '@mui/icons-material/CollectionsBookmarkRounded';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import RecentActorsRoundedIcon from '@mui/icons-material/RecentActorsRounded';
+import LanguageRoundedIcon from '@mui/icons-material/LanguageRounded';
+// Importation des icônes utilisées dans le Header
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import NotificationsRoundedIcon from '@mui/icons-material/NotificationsRounded';
+
+// Objet contenant toutes les icônes
+const icons = {
+  profile: PersonOutlineRoundedIcon,
+  dashboard: DashboardRoundedIcon,
+  utilisateurs: PeopleAltRoundedIcon,
+  Référentiels: CollectionsBookmarkRoundedIcon,
+  missions: FeedOutlinedIcon,
+  clients: RecentActorsRoundedIcon,
+  notification: NotificationsNoneOutlinedIcon,
+  revue: GradingRoundedIcon,
+  settings: SettingsOutlinedIcon,
+  "logout": LogoutRoundedIcon,
+  platforme: LanguageRoundedIcon,
+
+  // Icônes ajoutées pour le Header
+  home: HomeRoundedIcon,
+  notifications: NotificationsRoundedIcon,
+};
+
+export default icons;
