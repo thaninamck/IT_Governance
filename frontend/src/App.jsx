a15010c254283678fd14a8182e79184b574cbc74
Manel MOHAND OUALI <manel.mohandouali@mazars.dz>
2025-01-23 09:29:58 +0100

    fichier css pour les composents

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import PopUp from './components/PopUp';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import './index.css'
@@ -8,6 +8,13 @@ import Header from './components/Header/Header';
 import HeaderBis from './components/Header/HeaderBis';
 import NotificationBar from './components/Notification/NotificationBar';
 import NotificationPopup from './components/Notification/NotificationPopup';
+import InputForm from './components/Forms/InputForm';
+import AppForm from './components/Forms/AppForm';
+import Remediation from './components/Forms/Remediation';
+import SuiviRemForm from './components/Forms/SuiviRemForm';
+import AddClientForm from './components/Forms/AddClientForm';
+import AddMissionForm from './components/Forms/AddMissionForm';
+import AddUserForm from './components/Forms/AddUserForm';
 function App() {
   
 
@@ -15,12 +22,21 @@ function App() {
     <>
        <div className="App">
         <Routes>
-          <Route path='/popup' element={<PopUp/>}/>
+          
           <Route path='/sideBar' element={<SideBar userRole="admin"/>}/>
           <Route path='/header' element={<Header/>}/>
           <Route path='/headerbis' element={<HeaderBis/>}/>
           <Route path='/notification' element={<NotificationBar/>}/>
           <Route path='/notificationpopup' element={<NotificationPopup/>}/>
+          <Route path='/inputform' element={<InputForm/>}/>
+          <Route path='/appform' element={<AppForm title="Ajouter une nouvelle application" />}/>
+          <Route path='/remediationForm' element={<Remediation title="Ajouter une Remédiation" />}/>
+          <Route path='/suiviRemForm' element={<SuiviRemForm title="Ajouter une Remédiation" />}/>
+          <Route path='/addclientForm' element={<AddClientForm title="Ajouter un nouveau client" />}/>
+          <Route path='/addmissionForm' element={<AddMissionForm title="Ajouter une mission" />}/>
+          <Route path='/adduserForm' element={<AddUserForm title="Ajouter un nouveau utilisateur" />}/>
+
+
           
           
         </Routes>
--- frontend/src/assets/Icons.js
+++ frontend/src/assets/Icons.js
@@ -11,12 +11,16 @@ import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import RecentActorsRoundedIcon from '@mui/icons-material/RecentActorsRounded';
 import LanguageRoundedIcon from '@mui/icons-material/LanguageRounded';
 
+
 // Icônes ajoutées pour le Header
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import NotificationsRoundedIcon from '@mui/icons-material/NotificationsRounded';
 
 // icon notification
 import CheckCircleOutlineRoundedIcon from "@mui/icons-material/CheckCircleOutlineRounded";
+
+//icon add 
+import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
 
 // Objet contenant toutes les icônes
 const icons = {
@@ -31,13 +35,14 @@ const icons = {
   settings: SettingsOutlinedIcon,
   "logout": LogoutRoundedIcon,
   platforme: LanguageRoundedIcon,
-
   // Icônes ajoutées pour le Header
   home: HomeRoundedIcon,
   notifications: NotificationsRoundedIcon,
-
   // icon ajouter pour la notification
   checkCircle: CheckCircleOutlineRoundedIcon,
+  // icon ajouter pour client form
+  addCircle : AddCircleOutlineRoundedIcon,
+
 };
 
 export default icons;
