77611b02af6b8f5268aedf0147b1769382858dd7
Thanina MECHERAK <thanina.mecherak@mazars.dz>
2025-01-26 14:24:48 +0100

    evidence list

---
+++ frontend/src/components/Evidences/EvidenceList.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import DeleteIcon from "@mui/icons-material/Delete";
+
+const EvidenceList = ({ files, onDelete }) => {
+
+  /* Fonction de suppression déjà définie dans la page parente commme ca 
+  const [files, setFiles] = useState([
+  { name: 'file1.txt', size: 2048 },
+  { name: 'file2.jpg', size: 4096 },
+  // autres fichiers
+]);
+
+const handleDelete = (index) => {
+  const updatedFiles = files.filter((_, i) => i !== index); // Filtrer le fichier à supprimer
+  setFiles(updatedFiles); // Mettre à jour l'état des fichiers
+};
+  
+  */
+
+  return (
+    <>
+      {files.map((file, index) => (
+        
+        <div key={index} className="flex overflow-y-auto relative w-full flex-col mt-2 justify-center gap-4 bg-white border border-gray-300 rounded-lg p-2">
+          {/* Détails du fichier */}
+          <div className="flex justify-between mx-4">
+            <p className="font-medium text-sm">{file.name}</p> {/* Utilisation du nom du fichier */}
+            {/* ici on va traiter la suppression durant la programmmation */}
+            <DeleteIcon
+              sx={{ color: "red", cursor: "pointer" }}
+              onClick={() => onDelete(index)} // Appel de la fonction onDelete avec l'index
+            />
+          </div>
+
+          {/* Taille du fichier */}
+          <p className="text-xs text-subfont-gray mx-4">
+            {(file.size / 1024 / 1024).toFixed(2)} MB {/* Calcul de la taille en Mo */}
+          </p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default EvidenceList;
