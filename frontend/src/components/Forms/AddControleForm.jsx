72a11425d4a814ea8a884aa870bc7396f3f1b682
Manel MOHAND OUALI <manel.mohandouali@mazars.dz>
2025-02-13 14:34:26 +0100

    modifier controleForm

--- frontend/src/components/Forms/AddControleForm.jsx
+++ frontend/src/components/Forms/AddControleForm.jsx
@@ -3,14 +3,35 @@ import './FormStyle.css';
 import Button from '../Button';
 import AddCommentRoundedIcon from '@mui/icons-material/AddCommentRounded';
 import InputForm from './InputForm';
+import SelectInput from './SelectInput';
 
 function AddControleForm({ title, isOpen, onClose, onControleCreated }) {
   if (!isOpen) return null; // Ne pas afficher le modal si isOpen est false
 
+  const typeOptions = [
+    { label: "Détéctif", value:"étéctif" },
+    { label: "Coréctif", value: "Coréctif" },
+  ];
+  const sourceOptions = [
+    { label: "iso27001", value:"iso27001" },
+    { label: "ITGC", value: "ITGC" },
+  ];
+  const majorProcOptions = [
+    { label: "T1", value:"T1" },
+    { label: "T2", value: "T2" },
+  ];
+  const subProcOptions = [
+    { label: "sub1", value:"sub1" },
+    { label: "sub2", value: "sub2" },
+  ];
   const [controleData, setControleData] = useState({
     code: '',
     controle: '',
     testScripts: [''], // Initialiser avec un champ vide
+    type:'',
+    source:'',
+    majorProc:'',
+    subProc:'',
   });
 
   const [comments, setComments] = useState({}); // Gestion des commentaires {index: "commentaire"}
@@ -67,16 +88,33 @@ function AddControleForm({ title, isOpen, onClose, onControleCreated }) {
 
         <p>{title}</p>
         {/* Code du contrôle */}
+        <div className="form-row">
         <InputForm
           type="text"
           label="Code du controle"
           placeholder="code"
-          width="150px"
+          width="130px"
           flexDirection="flex-col"
           value={controleData.code}
           onChange={(e) => setControleData({ ...controleData, code: e.target.value })}
         />
-
+        <SelectInput
+          label="Type du controle"
+          options={typeOptions}
+          value={controleData.type}
+          onChange={(e) => setControleData({ ...controleData, type: e.target.value })}
+          width="170px"
+          multiSelect={false}
+        />
+        <SelectInput
+          label="Source du controle"
+          options={sourceOptions}
+          value={controleData.source}
+          onChange={(e) => setControleData({ ...controleData, source: e.target.value })}
+          width="170px"
+          multiSelect={false}
+        />
+</div>
         
         {/* Description du contrôle */}
     
@@ -84,7 +122,7 @@ function AddControleForm({ title, isOpen, onClose, onControleCreated }) {
           type="text"
           label="Controle"
           placeholder="description"
-          width="630px"
+          width="540px"
           flexDirection="flex-col"
           value={controleData.controle}
           onChange={(e) => setControleData({ ...controleData, controle: e.target.value })}
@@ -137,7 +175,6 @@ function AddControleForm({ title, isOpen, onClose, onControleCreated }) {
               </div>
             ))}
           </div>
-
           <button
             type="button"
             onClick={addTestScript}
@@ -146,7 +183,24 @@ function AddControleForm({ title, isOpen, onClose, onControleCreated }) {
             + Ajouter
           </button>
         </div>
-
+        <div className="form-row">
+        <SelectInput
+          label="Major process"
+          options={majorProcOptions}
+          value={controleData.majorProc}
+          onChange={(e) => setControleData({ ...controleData, majorProc: e.target.value })}
+          width="250px"
+          multiSelect={false}
+        />
+        <SelectInput
+          label="Sub process"
+          options={subProcOptions}
+          value={controleData.subProc}
+          onChange={(e) => setControleData({ ...controleData, subProc: e.target.value })}
+          width="250px"
+          multiSelect={false}
+        />
+        </div>
         {/* Bouton Créer */}
         <div className="mt-6 text-right">
           <Button btnName="Créer" type="submit" />
