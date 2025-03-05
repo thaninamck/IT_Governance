0a8c889060685eafd4252d581d1be0961eb39283
Manel MOHAND OUALI <manel.mohandouali@mazars.dz>
2025-03-05 10:25:13 +0100

    modifier regler les permissions

--- frontend/src/components/InfosDisplay/AddScope.jsx
+++ frontend/src/components/InfosDisplay/AddScope.jsx
@@ -4,12 +4,15 @@ import NewAppForm from '../Forms/AppForm';
 import Table from '../Table';
 import DeleteOutlineRoundedIcon from '@mui/icons-material/DeleteOutlineRounded';
 import DecisionPopUp from '../PopUps/DecisionPopUp';
+import { useNavigate, useParams } from 'react-router-dom';
 
-function AddScope({ title, text, text1, onToggleForm, showForm,userRole }) {
+function AddScope({ title, text, text1, onToggleForm, showForm, userRole, missionId }) {
+
+
   const [applications, setApplications] = useState([
-    {id:1,nomApp:"USSD",description:'llllll',owner:'farid akbi',contact:'farid@gmail.com',couche:["Data Base","Application"]},
-    {id:2,nomApp:"New SNOC",description:'llllll',owner:'farid akbi',contact:'farid@gmail.com',couche:['Data Base']},
-    {id:3,nomApp:"CSV360°",description:'llllll',owner:'farid akbi',contact:'farid@gmail.com',couche:['Data Base','Application']}
+    { id: 1, nomApp: "USSD", description: 'lllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll', owner: 'farid akbi', contact: 'farid@gmail.com', couche: ["Data Base", "Application"] },
+    { id: 2, nomApp: "New SNOC", description: 'llllll', owner: 'farid akbi', contact: 'farid@gmail.com', couche: ['Data Base'] },
+    { id: 3, nomApp: "CSV360°", description: 'llllll', owner: 'farid akbi', contact: 'farid@gmail.com', couche: ['Data Base', 'Application'] }
   ]);
   const [isDeletePopupOpen, setIsDeletePopupOpen] = useState(false);
   const [selectedAppId, setSelectedAppId] = useState(null);
@@ -19,7 +22,7 @@ function AddScope({ title, text, text1, onToggleForm, showForm,userRole }) {
 
   const columnsConfig = [
     { field: 'nomApp', headerName: 'Nom', width: 170, editable: true },
-    { field: 'description', headerName: 'Description', editable: true, width: 220 },
+    { field: 'description', headerName: 'Description', editable: true, width: 220, expandable: true },
     { field: 'owner', headerName: 'Owner', width: 170 },
     { field: 'contact', headerName: 'Contact', width: 200 },
     {
@@ -29,7 +32,7 @@ function AddScope({ title, text, text1, onToggleForm, showForm,userRole }) {
       expandable: true,
       customRenderCell: (params) => {
         console.log("Couche value:", params.value); // Debugging line
-      
+
         const coucheValues = Array.isArray(params.value) ? params.value : [];
         return (
           <div className="flex -space-x-2">
@@ -57,29 +60,10 @@ function AddScope({ title, text, text1, onToggleForm, showForm,userRole }) {
       }
     },
     // Ajouter la colonne "actions" conditionnellement
-  ...((userRole === 'manager' || userRole === 'admin')
-  ? [{ field: 'actions', headerName: 'Action', width: 80 }]
-  : [])
+    ...((userRole === 'manager' || userRole === 'admin')
+      ? [{ field: 'actions', headerName: 'Action', width: 80 }]
+      : [])
   ];
-
-  // const handleAddApp = (app) => {
-  //   if (selectedApp) {
-  //     // Mise à jour de l'application existante
-  //     setApplications((prevApps) =>
-  //       prevApps.map((row) => (row.id === app.id ? app : row))
-  //     );
-  //     setSelectedApp(null);
-  //     onToggleForm();
-  //   } else {
-  //     // Ajout d'une nouvelle application
-  //     setApplications((prev) => [
-  //       ...prev,
-  //       { id: prev.length + 1, ...app, couche: Array.isArray(app.couche) ? app.couche : [app.couche] }
-  //     ]);
-  //     setShowDecisionPopup(true);
-  //   }
-    
-  // };
 
   const handleAddApp = (app) => {
     if (selectedApp) {
@@ -134,24 +118,33 @@ function AddScope({ title, text, text1, onToggleForm, showForm,userRole }) {
     { icon: <DeleteOutlineRoundedIcon sx={{ color: 'var(--alert-red)', marginRight: '5px' }} />, label: 'Supprimer', onClick: handleDeleteRow }
   ];
 
+
+  const navigate = useNavigate(); // Hook pour la navigation
+  const handleRowClick = (rowData) => {
+    // Naviguer vers la page de détails avec l'ID du contrôle dans l'URL
+    navigate(`/tablemission/${missionId}/${rowData.nomApp}`, { state: { AppData: rowData } });
+    console.log('Détails du app sélectionné:', rowData);
+  };
+
+
   return (
-    <div className="p-4 mb-6">
+    <div className="p-4 mb-1">
       <div className="flex items-center gap-2 mb-4">
         <PlusCircle className="text-[var(--blue-menu)] w-5 h-5" />
         <h2 className="text-l font-semibold text-[var(--blue-menu)]">{title}</h2>
         <hr className="flex-grow border-t border-[var(--blue-menu)]" />
       </div>
 
-      {(userRole ==='manager' || userRole==='admin')&&
-      <div className="flex flex-row items-center gap-4 pl-6">
-        <p className="text-[var(--status-gray)] text-s">{applications.length > 0 ? text1 : text}</p>
-        <button
-          onClick={onToggleForm}
-          className="px-4 py-2 border-none bg-[var(--blue-menu)] text-white text-sm font-medium rounded"
-        >
-          Ajouter
-        </button>
-      </div>}
+      {(userRole === 'manager' || userRole === 'admin') &&
+        <div className="flex flex-row items-center gap-4 pl-6">
+          <p className="text-[var(--status-gray)] text-s">{applications.length > 0 ? text1 : text}</p>
+          <button
+            onClick={onToggleForm}
+            className="px-4 py-2 border-none bg-[var(--blue-menu)] text-white text-sm font-medium rounded"
+          >
+            Ajouter
+          </button>
+        </div>}
 
       {showDecisionPopup && (
         <div className="absolute top-25 left-1/2 -translate-x-1/2 translate-y-1/4 z-50 ">
@@ -168,7 +161,7 @@ function AddScope({ title, text, text1, onToggleForm, showForm,userRole }) {
         <NewAppForm title={''} initialValues={selectedApp || {}} onAddApp={handleAddApp} onClose={() => setShowDecisionPopup(false)} />
       )}
 
-      
+
       {isDeletePopupOpen && (
         <div className="absolute top-100 left-1/2 -translate-x-1/2 z-50 mt-9">
           <DecisionPopUp
@@ -187,6 +180,7 @@ function AddScope({ title, text, text1, onToggleForm, showForm,userRole }) {
             columnsConfig={columnsConfig}
             rowsData={applications}
             checkboxSelection={false}
+            onRowClick={handleRowClick}
             headerBackground="var(--blue-nav)"
             rowActions={rowActions}
             onCellEditCommit={(params) => {
