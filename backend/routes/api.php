91d41b6088016d9a74c6b20f5f1d73e44b782982
Thanina MECHERAK <thanina.mecherak@mazars.dz>
2025-03-08 09:59:16 +0100

    manage logs

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,6 +9,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\UserController;
 use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\LogController;
+
 
 /*Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,31 +18,24 @@ use App\Http\Controllers\Api\V1\AuthController;
 
 
 
-Route::middleware(['auth:sanctum',AdminMiddleware::class])->prefix('v1')->controller(UserController::class)->group(function() {
-    Route::get('/users',  'index');
-Route::post('/insert-user',  'store');
-Route::post('/reset-user/{id}',  'resetUser');
+Route::middleware(['auth:sanctum', AdminMiddleware::class])->prefix('v1')->controller(UserController::class)->group(function () {
+    Route::get('/users', 'index');
+    Route::post('/insert-user', 'store');
+    Route::post('/reset-user/{id}', 'resetUser');
 
-Route::patch('/update-user/{id}',  'updateUser');
-Route::patch('/block-user/{id}',  'blockUser');
-Route::patch('/unblock-user/{id}',  'unblockUser');
-Route::delete('/user/{id}',  'deleteUser');
-Route::post('/insert-users',  'storeMultiple');
+    Route::patch('/update-user/{id}', 'updateUser');
+    Route::patch('/block-user/{id}', 'blockUser');
+    Route::patch('/unblock-user/{id}', 'unblockUser');
+    Route::delete('/user/{id}', 'deleteUser');
+    Route::post('/insert-users', 'storeMultiple');
+    Route::get('/logs',  'getUserActivityLogs');
 });
 
-Route::post('/login',action: [AuthController::class, 'login'])->middleware(CheckPasswordReset::class);
-Route::post('/logout',[AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::post('/changePassword',[AuthController::class, 'forceUpdatePassword'])->middleware('auth:sanctum');
-Route::post('/check-email',[AuthController::class, 'checkEmailExists']);
+Route::post('/login', action: [AuthController::class, 'login'])->middleware(CheckPasswordReset::class);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/changePassword', [AuthController::class, 'forceUpdatePassword'])->middleware('auth:sanctum');
+Route::post('/check-email', [AuthController::class, 'checkEmailExists']);
 Route::post('/store-reset-code', [AuthController::class, 'storeResetCode']);
 Route::post('/verify-reset-code', [AuthController::class, 'verifyResetCode']);
 Route::post('/reset-password', [AuthController::class, 'resetPassword']);
-
-Route::patch('/block-user/{id}',  [UserController::class,'blockUser']);
-Route::patch('/unblock-user/{id}',  [UserController::class,'unblockUser']);
-Route::patch('/update-user/{id}',  [UserController::class,'updateUser']);
-Route::patch('/insert-user',  [UserController::class,'store']);
-Route::post('/insert-users',  [UserController::class,'storeMultiple']);
-Route::post('/reset-user/{id}',  [UserController::class,'resetUser']);
-Route::delete('/user/{id}',  [UserController::class,'deleteUser']);
 
