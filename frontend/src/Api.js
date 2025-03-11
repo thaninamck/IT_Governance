9e5f6720e57e713c1149010f71b46b700f79ea20
Manel MOHAND OUALI <manel.mohandouali@mazars.dz>
2025-03-11 12:13:39 +0100

    integrate getmissions v1

---
+++ frontend/src/Api.js
@@ -0,0 +1,12 @@
+
+// service Axios  pour gérer les appels API
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://127.0.0.1:8000/api/v1', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export default api;
