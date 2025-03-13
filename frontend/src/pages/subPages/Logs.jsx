1e2ecb9294033ca45c683fae6b9ebc1171bf450a
Manel MOHAND OUALI <manel.mohandouali@mazars.dz>
2025-03-13 14:54:53 +0100

    integrer logs

--- frontend/src/pages/subPages/Logs.jsx
+++ frontend/src/pages/subPages/Logs.jsx
@@ -1,24 +1,54 @@
 import { Tab } from '@mui/material';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Table from '../../components/Table';
 import ExportButton from '../../components/ExportButton';
+import api from '../../Api';
 
 function Logs() {
     // Configuration des colonnes de la table
+  // const columnslogs = [
+  //   { field: 'dateField', headerName: 'Date_Heure', width: 180, editable: false },
+  //   { field: 'utilisateur', headerName: 'utilisateur', width: 160, editable: true },
+  //   { field: 'macAddress', headerName: 'Mac address', width: 150 },
+  //   { field: 'profile', headerName: 'Profile', width: 180 },
+  //   { field: 'event', headerName: 'Action', width: 260, expandable: true, maxInitialLength: 20 },
+  //   { field: 'mission', headerName: 'Mission', width: 120 },
+  // ];
+  const [logs, setLogs] = useState([]);
   const columnslogs = [
-    { field: 'dateField', headerName: 'Date_Heure', width: 180, editable: false },
-    { field: 'utilisateur', headerName: 'utilisateur', width: 160, editable: true },
-    { field: 'macAddress', headerName: 'Mac address', width: 150 },
+    { field: 'date_heure', headerName: 'Date_Heure', width: 180 },
+    { field: 'utilisateur', headerName: 'Utilisateur', width: 160 },
+    { field: 'ip', headerName: 'ip Address', width: 150 },
+    { field: 'mac', headerName: 'Mac Address', width: 150 },
     { field: 'profile', headerName: 'Profile', width: 180 },
-    { field: 'event', headerName: 'Action', width: 260, expandable: true, maxInitialLength: 20 },
+    { field: 'action', headerName: 'Action', width: 260, expandable: true },
     { field: 'mission', headerName: 'Mission', width: 120 },
   ];
+   // Fonction pour récupérer les logs depuis l'API
+   const fetchLogs = async () => {
+    try {
+      const response = await api.get('/logs'); // Appel API
+      console.log(response.data)
+      setLogs(response.data); // Stockage des logs
+       setFilteredRows(response.data); // Remplissage des données affichées
+//       const fetchedLogs = response.data?.data || [];
+// setLogs(fetchedLogs); 
+// setFilteredRows(fetchedLogs);
+
+    } catch (error) {
+      console.error('Erreur lors de la récupération des logs:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchLogs();
+  }, []);
 
   // Données initiales des utilisateurs
   const rowslogs = [
-    { id: 1, dateField: '2024-12-23', utilisateur: 'houda.elmaouhab', macAddress: 'A3:FF:43:87', profile: 'manager', event: 'Clôture de la mission', mission: 'DSP'},
-    { id: 2, dateField: '2024-11-3', utilisateur: 'sara.lounes', macAddress: 'A3:AF:53:87', profile: 'superviseur', event: 'valider mission', mission: 'DSP1'},
-    { id: 3, dateField: '2024-1-13', utilisateur: 'houda.elmaouhab', macAddress: 'A3:FF:43:87', profile: 'Testeur', event: 'Submit controle', mission: 'DSP3'},
+    // { id: 1, dateField: '2024-12-23', utilisateur: 'houda.elmaouhab', macAddress: 'A3:FF:43:87', profile: 'manager', event: 'Clôture de la mission', mission: 'DSP'},
+    // { id: 2, dateField: '2024-11-3', utilisateur: 'sara.lounes', macAddress: 'A3:AF:53:87', profile: 'superviseur', event: 'valider mission', mission: 'DSP1'},
+    // { id: 3, dateField: '2024-1-13', utilisateur: 'houda.elmaouhab', macAddress: 'A3:FF:43:87', profile: 'Testeur', event: 'Submit controle', mission: 'DSP3'},
   ];
   const [filteredRows, setFilteredRows] = useState(rowslogs);
   return (
@@ -31,7 +61,9 @@ function Logs() {
         </div>
         <div className="flex-1 mt-6 overflow-x-auto overflow-y-auto h-[500px]">
           <Table
-            key={filteredRows.length}
+            // key={filteredRows.length}
+            key={filteredRows?.length || 0}
+
             columnsConfig={columnslogs}
             rowsData={filteredRows}
             checkboxSelection={false}
