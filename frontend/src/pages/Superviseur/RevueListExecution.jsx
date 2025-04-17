0112ac3c48d81dc4d3fdd9f50f9f5f044c914cd5
Manel MOHAND OUALI <manel.mohandouali@mazars.dz>
2025-04-18 00:05:42 +0100

    revue version 0 il rest la gestion du profile manager superviseur et qlq detail front

--- frontend/src/pages/Superviseur/RevueListExecution.jsx
+++ frontend/src/pages/Superviseur/RevueListExecution.jsx
@@ -60,6 +60,7 @@ function RevueListExecution() {
                 }
               };
           
+              
               return (
                 <div
                   onClick={handleCopy}
