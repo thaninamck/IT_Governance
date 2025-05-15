5d4cfbf898bedad4a8f7be2e160eaad838323ead
Thanina MECHERAK <thanina.mecherak@mazars.dz>
2025-05-15 08:51:20 +0100

    correcting the forms problem

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import "./componentStyle.css";
 import Spinner from "../components/Spinner";
+import { CircularProgress, Box } from "@mui/material";
 
-function Button({ loading, btnName, disabled }) {
+function Button({ color = "#FFFFFF", size = 40,loading, btnName, disabled }) {
   return (
     <button className="save-button" disabled={disabled}>
       {loading ? (
         <div className="flex items-center justify-center w-full h-full">
-          <Spinner color="var(--blue-menu)" />
+          <CircularProgress style={{ color }} size={size} />
         </div>
       ) : (
         btnName
