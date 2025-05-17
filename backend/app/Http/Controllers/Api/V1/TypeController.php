88fdd8d4d2d275c66b2d8590577d1228586c90f9
Manel MOHAND OUALI <manel.mohandouali@mazars.dz>
2025-05-17 14:50:18 +0100

    gestion type controls

--- backend/app/Http/Controllers/Api/V1/TypeController.php
+++ backend/app/Http/Controllers/Api/V1/TypeController.php
@@ -3,25 +3,88 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\V1\TypeResource;
 use App\Models\Type;
+use App\Services\LogService;
+use App\Services\V1\TypeService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
-class TypeController extends Controller
+class TypeController extends BaseController
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+
+    protected $typeService;
+    protected $logService;
+    
+    public function __construct(TypeService $typeService ,LogService $logService)
     {
-        //
+        $this->logService=$logService;
+        $this->typeService=$typeService;
+    }
+    public function index():JsonResponse
+    {
+        $types=$this->typeService->getAllTypes();
+
+        if ($types->isEmpty()){
+
+            return $this->sendError("no type found",[]);
+        }
+        return $this->sendResponse(TypeResource::collection($types),"type retrived successfully");
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function store(Request $request):JsonResponse
     {
-        //
+        try{
+            $rules = [
+                'name' => 'required|string|max:255', 
+            ];
+    
+            $validator =Validator::make($request->all(),$rules);
+
+            if($validator->fails()){
+                return $this->sendError("validation failed",$validator->errors(),422);
+            }
+
+            $typeData=$validator->validated();
+
+            $type = $this->typeService->createType($typeData['name']);
+
+            $this->logService->logUserAction(
+                auth()->user()->email ??'Unknown',
+                'Admin',
+                "Création d'un type de controle {$type->name}",
+                "" 
+            );
+            $response=[
+                'type' =>new TypeResource($type),
+                'message'=>'type created successfully'
+            ];
+            return $this->sendResponse($response,"type created successfully",201);
+        }catch(\Exception $e){
+            return $this->sendError("An error occured",["error"=>$e->getMessage()],500);
+        }
+    }
+
+    public function deleteType($id):JsonResponse
+    {
+        try{
+            $name =$this->typeService->deleteType($id);
+            if(!$name){
+                return $this->sendError("type not found",[],404);
+            }
+            $this->logService->logUserAction(
+                auth()->user()->email ?? 'Unknown',
+                'Admin',
+                "Suppression  d'un type de control:{$name}",
+                " "
+            );    
+            return $this->sendResponse(['success'=>true],"type deleted successfully");
+
+        }catch(\Exception $e)
+        {
+            return $this->sendError("an erroe occured",['error'=>$e->getMessage()],500);
+        }
     }
 
     /**
--- backend/app/Services/V1/TypeService.php
+++ backend/app/Services/V1/TypeService.php
@@ -6,6 +6,7 @@ use App\Repositories\V1\ControlRepository;
 use App\Models\Control;
 use App\Models\SubProcess;
 use App\Models\MajorProcess;
+use App\Models\Type;
 use App\Repositories\V1\MajorProcessRepository;
 use App\Repositories\V1\TypeRepository;
 
@@ -18,6 +19,28 @@ public function __construct(TypeRepository $typeRepository){
 public function firstOrCreate(array $data)
 {
     return $this->typeRepository->firstOrCreate($data);
+}
+
+public function getAllTypes(){
+    return $this->typeRepository->getAllTypes();
+}
+public function createType(string $name):Type
+{
+    // Vérifier si une couche avec le même nom existe déjà (en ignorant la casse)
+    $existingType = Type::whereRaw('LOWER(name) = ?', [strtolower($name)])->first();
+
+    if ($existingType) {
+        throw new \Exception("Un type avec le même nom existe déjà.");
+    }
+    return $this->typeRepository->createType($name);
+}
+public function deleteType(int $id) :?string
+{
+    $type=$this->typeRepository->findTypeById($id);
+    if(!$type){
+        return null;
+    }
+    return $this->typeRepository->deleteType($id);
 }
 
 }
