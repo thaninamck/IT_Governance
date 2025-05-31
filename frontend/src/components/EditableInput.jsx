6116a173199e5822a8a29a567ee127e7c058e142
Manel MOHAND OUALI <manel.mohandouali@mazars.dz>
2025-05-31 03:11:50 +0100

    supprimer button modifier de editInput composent

--- frontend/src/components/EditableInput.jsx
+++ frontend/src/components/EditableInput.jsx
@@ -40,14 +40,14 @@ const EditableTextarea = ({ placeholder = 'Saisir un commentaire ...', onSave ,c
         rows={3}
         className="w-full py-2 px-4 pr-20 text-gray-700 placeholder-gray-400 resize-none focus:outline-none focus:ring focus:ring-blue-300"
       />
-      {showButton && (
+      {/* {showButton && (
         <button
           onClick={handleSave}
           className="absolute bottom-2 right-2 bg-[var(--blue-menu)] text-white text-sm font-medium py-1 px-3 rounded-lg shadow hover:bg-blue-700 transition-all border-none"
         >
           Modifier
         </button>
-      )}
+      )} */}
     </div>
   );
 };
