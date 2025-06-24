25e3b06391cff94215888f202bcb007b6cc5e4ef
Manel MOHAND OUALI <manel.mohandouali@mazars.dz>
2025-06-24 18:20:31 +0100

    style

--- frontend/src/pages/GestionMission.jsx
+++ frontend/src/pages/GestionMission.jsx
@@ -404,7 +404,7 @@ function GestionMission() {
         />
       )}
       {isDeletePopupOpen && (
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1 z-[9999]">
           {" "}
           <DecisionPopUp
             name={
